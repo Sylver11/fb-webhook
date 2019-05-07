97f5e76493d3f5b50b48a975d6ef3db2a961f30b
Sylver11 <justus.voigt@spellgeek.com>
2019-05-07 22:24:59 +0200

    debugging

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.post('/webhook', (req, res) => {
 
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
+      console.log(entry);
       console.log(webhook_event);
 
 
